685031f7f145bdbcebdf2d310378fc6aee4d1ab0
mgkim <dxeng@wise.co.kr>
2023-01-03 08:24:32 +0900

    Login_FindEmail complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Main from "./pages/Main.jsx";
+import Footer from "./pages/Footer.jsx";
+// import Sign from "./pages/Sign.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_app">
+      <Routes>
+        <Route path='*' element={<Main />} />
+        {/* <Route path='/sign' element={<Sign />} /> */}
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/Sign.jsx
@@ -0,0 +1,19 @@
+// function Sign() {
+//     return (
+//         <>
+//             <div className="signBox">
+//                 <div className="title">
+//                     설계정보 디지털 변환 플랫폼
+//                     <br />
+//                     회원 가입
+//                     <br />
+//                     약관 동의
+//                 </div>
+//                 <div className="content">
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default Sign;
